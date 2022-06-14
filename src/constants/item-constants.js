d14b48044da66d87630ce1d570d81df5a1e07ad2
shao.ruichen <shao.ruichen@linecorp.com>
2022-06-14 14:30:49 +0800

    todo list v1

---
+++ src/constants/item-constants.js
@@ -0,0 +1,2 @@
+export const ItemStateFinished=2
+export const ItemStateUnFinished=1
---
+++ vue.config.js
@@ -0,0 +1,20 @@
+const path = require('path')
+
+module.exports = {
+  devServer: {
+    port: 8081,
+    proxy: {
+      '/': {
+        target: process.env.VUE_APP_BACKEND_TARGET,
+        changeOrigin: true,
+      }
+    }
+  },
+  configureWebpack: {
+    resolve: {
+      alias: {
+        '@': path.join(__dirname, './src'),
+      }
+    },
+  },
+}
